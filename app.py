6dc9c7cce2209295302c9a3e382a02737d72a6fe
CB-BIUS <67938171+CB-BIUS@users.noreply.github.com>
2020-07-31 16:03:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
- app = Flask(__name__)
-
-
- @app.route('/')
- def index():
-     return render_template('index.html')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/cameronbaffuto/Desktop/fileupload/filestorage.db'
+db = SQLAlchemy(app)
 
 
- @app.route('/upload', methods=['POST'])
- def upload():
-     file = request.files['inputFile']
-     return file.filename
+class FileContents(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(300))
+    data = db.Column(db.LargeBinary)
 
 
- if __name__ == '__main__':
-     app.run()
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['inputFile']
+
+    newFile = FileContents(name=file.filename, data=file.read())
+    db.session.add(newFile)
+    db.session.commit()
+
+    return 'Saved ' + file.filename + ' to the database!'
+
+
+if __name__ == '__main__':
+    app.run()
